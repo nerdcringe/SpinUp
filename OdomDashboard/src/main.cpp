7db0a95a0c6afbf940b78f0c9cb35797eb9ee8da
ripWaterSheep <54510965+ripWaterSheep@users.noreply.github.com>
2022-07-19 19:27:06 -0400

    Cleaned up a bit

--- OdomDashboard/src/main.cpp
+++ OdomDashboard/src/main.cpp
@@ -29,17 +29,13 @@ double lastForwardReading = 0;
 double lastSidewaysReading = 0;
 double lastRotation = 0;
 
-/*
-// new version (7/15)
-double lastEncoderL = 0;
-double lastEncoderR = 0;
-*/
 
 double globalXPos = 0;
 double globalYPos = 0;
 
-double desiredX = 0;
-double desiredY = 0;
+double targetX = 0;
+double targetY = 0;
+float targetAngle = 0;
 
 
 // DEVICES ///////////////////////////////////
@@ -103,6 +99,45 @@ double keepAngleIn180(double a)
 };
 
 
+
+// screen is 480 x 272
+void drawPoint(double x, double y)
+{
+    double tile_size = 30;
+    double draw_pos_x = ((x / 24) * tile_size) + 105;
+    double draw_pos_y = ((-y / 24) * tile_size) + 105;
+    
+    Brain.Screen.setFillColor(purple);
+    Brain.Screen.setPenWidth(0);
+    Brain.Screen.drawCircle(draw_pos_x, draw_pos_y, 4);
+    Brain.Screen.setFillColor(color(10, 80, 30)); // green in rgb
+}
+
+
+// Convert where the screen is tapped to field coordinates
+double screenToGlobalX(double screenX)
+{
+    double tile_size = 30;
+    double globalX = ((screenX - 105) / tile_size) * 24;
+
+    /*double draw_pos_x = ((x/24) * tile_size) + 105;
+    double draw_pos_y = ((y/24) * tile_size) + 105;*/
+    return globalX;
+}
+
+// Convert where the screen is tapped to field coordinates
+double screenToGlobalY(double screenY)
+{
+    double tile_size = 30;
+    double globalY = -((screenY - 105) / tile_size) * 24;
+
+    return globalY;
+}
+
+
+
+
+
 // SENSOR FUNCTIONS ///////////////////////////////////////////////////////
 
 double getForwardReading() {
@@ -127,6 +162,7 @@ double getRadians()
 {
   return getDegrees() * toRadians;
 }
+
 
 
 
@@ -167,6 +203,61 @@ void stopBase()
 }
 
 
+float maxTurnPower = 20;
+float finalTurnPower = 15;
+float Kp = 0.3;
+float Ki = 0.02;
+float Kd = 0.075;
+
+float turnError = 0;
+float lastTurnError = 0;
+float turnIntegral = 0;
+float turnDerivative = 0;
+
+float integralPowerLimit =
+    40 / Ki;                   // little less than half power in pct (percent)
+float integralActiveZone = 15; // degrees b/c its a gyro turn doesnt use ticks
+
+float turnErrorThreshold = 0.75; // Exit loop when error is less than this
+
+
+void updateTurnPID()
+{
+
+  turnError = targetAngle - getDegrees();
+
+  if (fabs(turnError) > turnErrorThreshold)
+  {
+    if (fabs(turnError) < integralActiveZone) {
+      turnIntegral = turnIntegral + turnError;
+    } else {
+      turnIntegral = 0;
+    }
+    turnIntegral = keepInRange(turnIntegral, -integralPowerLimit, integralPowerLimit);
+
+    turnDerivative = turnError - lastTurnError;
+
+    finalTurnPower = ((Kp * turnError) + (Ki * turnIntegral) + (Kd * turnDerivative));
+    finalTurnPower =  keepInRange(finalTurnPower, -maxTurnPower, maxTurnPower);
+    Brain.Screen.printAt(210, 120, "P: %.1f, I: %.1f, D: %.1f", (Kp * turnError), (Ki * turnIntegral), (Kd * turnDerivative));
+  }
+  else
+  {
+    turnIntegral = 0;
+    turnDerivative = 0;
+    finalTurnPower = 0;
+  Brain.Screen.printAt(210, 120, "PID Turn Completed             ");
+  }
+  lastTurnError = turnError;
+  
+
+  vex::task::sleep(40);
+}
+
+
+
+
+// ODOMETRY /////////////////////////////////////////////////////////////////////////////
 
 // Run one odometry calculation to update global position
 // Run in a parallel task during auton
@@ -223,82 +314,7 @@ void updatePosition()
 }
 
 
-// screen is 480 x 272
-void drawPoint(double x, double y)
-{
-    double tile_size = 30;
-    double draw_pos_x = ((x / 24) * tile_size) + 105;
-    double draw_pos_y = ((-y / 24) * tile_size) + 105;
-    
-    Brain.Screen.setFillColor(purple);
-    Brain.Screen.setPenWidth(0);
-    Brain.Screen.drawCircle(draw_pos_x, draw_pos_y, 4);
-    Brain.Screen.setFillColor(color(10, 80, 30)); // green in rgb
-}
 
-// Convert where the screen is tapped to field coordinates
-double screenToGlobalX(double screenX)
-{
-    double tile_size = 30;
-    double globalX = ((screenX - 105) / tile_size) * 24;
-
-    /*double draw_pos_x = ((x/24) * tile_size) + 105;
-    double draw_pos_y = ((y/24) * tile_size) + 105;*/
-    return globalX;
-}
-
-// Convert where the screen is tapped to field coordinates
-double screenToGlobalY(double screenY)
-{
-    double tile_size = 30;
-    double globalY = -((screenY - 105) / tile_size) * 24;
-
-    return globalY;
-}
-
-
-
-float targetAngle = 0;
-float maxPower = 15;
-float Kp = 0.5;
-float Ki = 0.0;
-float Kd = 0.0;
-
-float error = 0;
-float lastError = 0;
-float integral = 0;
-float derivative = 0;
-
-float integralPowerLimit =
-    40 / Ki;                   // little less than half power in pct (percent)
-float integralActiveZone = 15; // degrees b/c its a gyro turn doesnt use ticks
-
-float exitThreshold = 0.75; // Exit loop when error is less than this
-
-
-void updatePID()
-{
-  error = targetAngle - getDegrees();
-
-    if (fabs(error) < integralActiveZone && error != 0) {
-      integral = integral + error;
-    } else {
-      integral = 0;
-    }
-    integral = keepInRange(integral, -integralPowerLimit, integralPowerLimit);
-
-    derivative = error - lastError;
-    lastError = error;
-
-    float finalPower = ((Kp * error) + (Ki * integral) + (Kd * derivative));
-    finalPower =  keepInRange(finalPower, -maxPower, maxPower);
-
-    leftDrive(-finalPower);
-    rightDrive(finalPower); 
-    Brain.Screen.printAt(210, 120, "P: %.2f, I: %.2f, D: %.2f", (Kp * error), (Ki * integral), (Kd * derivative));
-
-    vex::task::sleep(40);
-}
 
 
 
@@ -306,229 +322,42 @@ void updatePID()
 void turnToPoint(double turnSpeed)
 {
   
-  drawPoint(desiredX, desiredY);
+  drawPoint(targetX, targetY);
 
-  double relativeX = desiredX - globalXPos; // error = desired - actual
-  double relativeY = desiredY - globalYPos;
+  double relativeX = targetX - globalXPos; // error = desired - actual
+  double relativeY = targetY - globalYPos;
   double angleToPosition;
 
 
   //while (true)
   {
-    double speedL = 0;
-    double speedR = 0;
-
-    // Avoid dividing by zero when doing atan
-    /*if (relativeX == 0)
-    {
-      angleToPosition = 90;
-    }
-    else
-    {
-      angleToPosition = toDegrees * atan(relativeY/relativeX); // get angle from opposite/adjacent ratio
-    }*/
 
     double currentAngle = getDegrees();
-    // Keep currentAngle within -180 to 180
-    //currentAngle = keepAngleIn180(currentAngle);
 
     // atan2 gives the angle to any position relative to the robot
     angleToPosition = toDegrees * atan2(relativeY, relativeX);
 
-    // Wraparound angle so that it is the closest to the current rotation
-
-    // The angles go from -180 to 180
-    // If the robot is at 179 and wants to go to -179, it would go all the way around instead of going 2 degrees the other way
-    // Thi
-
-    // Prevents the robot from going all the way around if  it is closer to go the other way
+    // Prevent the robot from targeting a rotation over 180 degrees from its current rotation.
+    // If it's more than 180 it's faster to turn the other direction
     double closestAngle = angleToPosition;
-    while(closestAngle > currentAngle + 180)
+    while(closestAngle > currentAngle + 180) // Subtract 360 if angle is too large
     {
-      // Adding/subtracting 360 doesn't change the angle, the robot just needs to rotate a different amount to get there
       closestAngle -= 360;
     }
-    while(closestAngle < currentAngle - 180)
+    while(closestAngle <= currentAngle - 180) // Add 360 if angle is too large
     {
       closestAngle += 360;
     }
-
-    // https://stackoverflow.com/questions/28909231/how-to-find-the-nearest-angle-from-0-to-180-and-180-to-0
-    
-    // get the closest rotation of any revolution (since + or - 360 is the same angle)
-    /*
-    
-    double error = angleToPosition - currentAngle; // how far off from angle (desired - actual)
-
-    double closestError = fmin(fabs(error), 360 - fabs(error));
-    if (closestError < error)
-    {
-      error = closestError;
-    }
-
-    targetAngle = error + currentAngle;*/
-
-    
-    // atan2 only seems to return angles within 0-360, but it's possible an angle outside that range is closer
-    // compare the angle to the same angle but 1 revolution behind or ahead to get the closest  angle
-
-    /*int revAhead = angleToPosition + 360;
-    int revBehind = angleToPosition - 360;
-    
-    //  Brain.Screen.printAt(230, 140, "0: %d A: %d B: %d         ", (int)round(angleToPosition), (int)round(revAhead), (int)round(revBehind));
-    if (fabs(revAhead - currentAngle) < fabs(angleToPosition - currentAngle))
-    {
-      angleToPosition = revAhead;
-      Brain.Screen.printAt(230, 160, "+");
-    }
-    else if (fabs(revBehind - currentAngle) < fabs(angleToPosition - currentAngle))
-    {
-      angleToPosition = revBehind;
-      Brain.Screen.printAt(230, 160, "-");
-    }
-    else
-    {
-      
-      Brain.Screen.printAt(230, 160, "0");
-    }*/
-
     targetAngle = closestAngle;
 
-    //Brain.Screen.printAt(210, 100, "angleToPosition: %.1f   ", angleToPosition);
-
     Brain.Screen.printAt(210, 100, "closestAngle: %.1f   ", closestAngle);
-    
-    // distance from the targetAngle
-
-
-                    /*double positionError =  sqrt(pow(desiredX - globalXPos, 2) + pow(desiredY - globalYPos, 2));
-                    
-                    if (positionError > 5) // go back if too far forward
-                    {
-                      RSpeed = moveSpeed;
-                      LSpeed = moveSpeed;
-                    }
-                    else if (positionError < -5) // go forward if too far back
-                    {
-                      RSpeed = -moveSpeed;
-                      LSpeed = -moveSpeed;
-                    }
-                */
-    /*double angleError = angleToPosition - getDegrees(); // how far off from angleToPosition the robot is
-    if (angleError > 1) // turn left if rotation too far left
-    {
-      speedR += turnSpeed;
-      speedL -= turnSpeed;
-    }
-    else if (angleError < -1) // turn right if rotation too far left 
-    {
-      speedR -= turnSpeed;
-      speedL += turnSpeed;
-    }
-
-    leftDrive(speedL);
-    rightDrive(speedR);*/
-    
-    //updatePID();
   }
 }
 
 
 
+// DISPLAY ///////////////////////////
 
-/*
-void turnPID(float targetAngle, float maxPower) {
-
-  float Kp = 0.48;
-  float Ki = 0.005;
-  float Kd = 0.499;
-  float change = 1.0;
-
-  float error = (targetAngle * change) - getDegrees();
-  float lastError;
-  float integral;
-  float derivative;
-
-  float integralPowerLimit =
-      40 / Ki;                   // little less than half power in pct (percent)
-  float integralActiveZone = 15; // degrees b/c its a gyro turn doesnt use ticks
-  // explaining integral active zone
-  // area where proportion is effective is represented with >>/<<
-  // area where proportion is not strong enough to move base rep with ----
-  // 0 is the target where error is equal to 0
-  // only want integral during ineffective zone to prevent windup of power
-  //>>>>>>>>>>>>-----------0---------------<<<<<<<<<<<<<<<<
-
-  float exitThreshold = 0.75; // Exit loop when error is less than this
-  float finalPower;
-
-  Brain.resetTimer();
-
-  while (fabs(error) > exitThreshold) {
-    error = (targetAngle * change) - getDegrees();
-
-    if (fabs(error) < integralActiveZone && error != 0) {
-      integral = integral + error;
-    } else {
-      integral = 0;
-    }
-    integral = keepInRange(integral, -integralPowerLimit, integralPowerLimit);
-
-    derivative = error - lastError;
-    lastError = error;
-
-    finalPower = ((Kp * error) + (Ki * integral) + (Kd * derivative));
-    finalPower =  keepInRange(finalPower, -maxPower, maxPower);
-
-    leftDrive(-finalPower);
-    rightDrive(finalPower); 
-    Brain.Screen.printAt(210, 120, "P: %.2f, I: %.2f, D: %.2f", (Kp * error),
-                         (Ki * integral), (Kd * derivative));
-
-    vex::task::sleep(40);
-  }
-  stopBase();
-}*/
-
-
-
-
-
-/*
-  // New way that accounts for position of tracking wheels
-  // https://www.youtube.com/watch?v=qqODIdvSGac&t=417s
-  void updatePosition2()
-  {
-    //double currentEncoderX = getEncoderX();
-    double currentEncoderY = getEncoderY();
-
-    double radians = getRadians();
-
-
-
-
-    double currentEncoderYR = getEncoderYR();
-    double YRChange = currentEncoderYR - lastEncoderYR; // Calculate change in right Y encoder as an arc
-    double centerYChange = (YRChange / radians) + TR; // Calculate center arc length (right arc + right offset from center)
-
-    double currentEncoderX = getEncoderX();
-    double xChange = currentEncoderX - lastEncoderX; // Calculate change in x encoder as arc
-    double centerXChange = (xChange / radians) + TS;// Calculate center arc length (back arc + back offset from center)
-
-
-    // Multiply x and y by this to make the angle correct
-    double angleMultiplier = 2 * sin(radians / 2);
-    globalYPos += angleMultiplier * centerYChange;
-    globalXPos += angleMultiplier * centerXChange;
-
-    //lastEncoderYL = currentEncoderYL;
-    lastEncoderYR = currentEncoderYR;
-    lastEncoderX = currentEncoderX;
-  }
-*/
-
-
-// ART ///////////////////////////
 
 // Draw the dashboard to visually display the robot's location and position
 void draw()
@@ -599,16 +428,16 @@ int main()
 
     if (Brain.Screen.pressing())
     {
-      desiredX = screenToGlobalX(Brain.Screen.xPosition());
-      desiredY = screenToGlobalY(Brain.Screen.yPosition());
+      targetX = screenToGlobalX(Brain.Screen.xPosition());
+      targetY = screenToGlobalY(Brain.Screen.yPosition());
     }
     turnToPoint(10);
 
-    Brain.Screen.printAt(210, 120, "Desired: (%.1f, %.1f)", desiredX, desiredY);
+    Brain.Screen.printAt(210, 120, "Desired: (%.1f, %.1f)", targetX, targetY);
 
 
 
-    error = targetAngle - getDegrees();
+    /*error = targetAngle - getDegrees();
 
     if (fabs(error) < integralActiveZone && error != 0) {
       integral = integral + error;
@@ -621,10 +450,12 @@ int main()
     lastError = error;
 
     float turnPower = ((Kp * error) + (Ki * integral) + (Kd * derivative));
-    turnPower =  keepInRange(turnPower, -maxPower, maxPower);
+    turnPower =  keepInRange(turnPower, -maxTurnPower, maxTurnPower);
 
-    Brain.Screen.printAt(210, 140, "P: %.2f, I: %.2f, D: %.2f", (Kp * error), (Ki * integral), (Kd * derivative));
+    Brain.Screen.printAt(210, 140, "P: %.2f, I: %.2f, D: %.2f", (Kp * error), (Ki * integral), (Kd * derivative));*/
 
+    
+    updateTurnPID();
 
 
     double driveSpeed = 0;
@@ -637,8 +468,8 @@ int main()
       driveSpeed = -10;
     }
 
-    leftDrive(driveSpeed + turnPower);
-    rightDrive(driveSpeed - turnPower);
+    leftDrive(driveSpeed + finalTurnPower);
+    rightDrive(driveSpeed - finalTurnPower);
     /*if (controllerPrim.ButtonUp.pressing())
     {
       drive(10);
